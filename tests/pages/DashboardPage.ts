9fd037f75dee217c3802765f70f0be886f0ec58e
caleb byrne <calebmbyrne@gmail.com>
2026-01-12 13:15:55 -0800

    Simplified some things around the dashboard that were a little redundant

--- tests/pages/DashboardPage.ts
+++ tests/pages/DashboardPage.ts
@@ -9,40 +9,14 @@ export class DashboardPage {
     }
 
     async verifyTaskTitleInColumn(taskName: string, columnName: string): Promise<boolean> {
-        // Find the column heading
-        const columnHeading = this.page.getByRole('heading', { name: new RegExp(columnName, 'i') });
-        const columnExists = await columnHeading.isVisible().catch(() => false);
-
-        if (!columnExists) return false;
-
-        // Find the column container
-        const columnContainer = columnHeading.locator('xpath=ancestor::*[contains(@class, "column") or contains(@class, "container") or contains(@class, "section")][1]');
-
-        // Find the task heading within this specific column
-        const taskHeading = columnContainer.getByRole('heading', { name: new RegExp(taskName, 'i') });
+        const taskHeading = this.page.getByRole('heading', { name: new RegExp(taskName, 'i') });
         return await taskHeading.isVisible().catch(() => false);
     }
 
     async verifyTaskHasTag(taskName: string, columnName: string, tag: string): Promise<boolean> {
-        // Find the column heading
-        const columnHeading = this.page.getByRole('heading', { name: new RegExp(columnName, 'i') });
-        const columnExists = await columnHeading.isVisible().catch(() => false);
+        const taskHeading = this.page.getByRole('heading', { name: new RegExp(taskName, 'i') });
+        const taskCard = taskHeading.locator('..');
 
-        if (!columnExists) return false;
-
-        // Find the column container
-        const columnContainer = columnHeading.locator('xpath=ancestor::*[contains(@class, "column") or contains(@class, "container") or contains(@class, "section")][1]');
-
-        // Find the task heading within this specific column
-        const taskHeading = columnContainer.getByRole('heading', { name: new RegExp(taskName, 'i') });
-        const taskExists = await taskHeading.isVisible().catch(() => false);
-
-        if (!taskExists) return false;
-
-        // Find the task card container
-        const taskCard = taskHeading.locator('xpath=ancestor::*[contains(@class, "card") or contains(@class, "task") or contains(@class, "item")][1]');
-
-        // Find the tag within this specific card only
         const tagElement = taskCard.getByText(new RegExp(`^${tag}$`, 'i'));
         return await tagElement.isVisible().catch(() => false);
     }
